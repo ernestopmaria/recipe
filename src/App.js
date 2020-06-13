b9cc66f138a100c96d1ff7d3604be59563e03cac
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-14 00:33:54 +0100

    connecting our app to external api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
-function App() {
+const App =()=> {
+
+  const APP_ID = "8f809bfe";
+  const APP_KEY = "c2bd663000e60f666031d5b8c0c8eb8d";
+
+
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(()=>{
+  }, []);
+
+  const getRecipes = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <form className ="search-form">
+        <input className="search-bar" type="text"/>
+        <button  className ="search-botton" type="submit">
+          Search
+          </button>
+ 
+      </form>
     </div>
   );
 }
